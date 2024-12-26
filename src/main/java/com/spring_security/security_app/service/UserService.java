6c6e875c2bb17adb140821486d28dc06bd068ea4
Naga Venkata Sairam Yanamadala <ynvsr.25@gmail.com>
2024-12-26 11:54:14 -0600

    generated JWT token

--- src/main/java/com/spring_security/security_app/service/UserService.java
+++ src/main/java/com/spring_security/security_app/service/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
 
         if(authentication.isAuthenticated())
-            return jwtService.generateToken();
+            return jwtService.generateToken(user.getUsername());
 
         return "fail";
     }
